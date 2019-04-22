46e8f267c5ba7708562f849729f4d3da05337dd8
Yoshitomo Minami <minami530@gmail.com>
2019-04-22 23:06:43 +0900

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,44 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.generatespectemplate', async () => {
+		const workspaces = vscode.workspace.workspaceFolders;
+
+		if (workspaces !== undefined) {
+			const specName = await vscode.window.showInputBox({
+				placeHolder: "Name Your Spec"
+			});
+
+			if (specName === undefined) {
+				vscode.window.showErrorMessage("Spec name is empty!!");
+				return;
+			}
+
+			const current = workspaces[0].uri.fsPath;
+			const root = path.join(current, specName);
+
+			fs.mkdir(root, err => {
+				if (err === null) {
+					fs.mkdir(path.join(root, 'images'), err => {});
+					fs.mkdir(path.join(root, 'materials'), err => {});
+					fs.appendFile(path.join(root, 'index.md'), "", err => {});
+				} else {
+					vscode.window.showErrorMessage(err.message);
+				}
+			});
+
+			vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.parse(root), false);
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
